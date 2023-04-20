3dab941907d8f93629170e8ee702b055eeccd87c
triple666killaz <blackrascal14@gmail.com>
2023-04-20 14:55:15 +0300

    Init commit

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static CSharpTest.WorkDayCalculatorTests;
+
+
+namespace CSharpTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*DateTime dateTime1 = DateTime.Now;
+        DateTime dateTime2 = DateTime.Today;
+        Console.WriteLine(dateTime1);
+        Console.WriteLine(dateTime2);
+        Console.WriteLine(dateTime1 < dateTime2);*/
+
+    }
+}
+
+
---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+// using System.Linq;
+using System.Text;
+// using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            if(weekEnds == null)
+                return startDate.AddDays(dayCount-1);
+            
+            dayCount += GetTotalWeekEndsAmount(weekEnds);
+
+            return startDate.AddDays(dayCount);
+        }
+
+        private int GetTotalWeekEndsAmount(WeekEnd[] weekEnds)
+        {
+            int amount = 0;
+
+            foreach (var weekEnd in weekEnds)
+                amount += (int)(weekEnd.EndDate - weekEnd.StartDate).TotalDays;
+            
+            return amount;
+        }
+    }
+}
